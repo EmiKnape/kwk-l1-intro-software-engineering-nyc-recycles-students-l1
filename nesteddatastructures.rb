c4bc8f8150cd8c24d12f379485deb0f3a59c6de6
EmiKnape <enknape@aol.com>
2018-06-08 20:11:39 +0000

    automatically backed up by learn

---
+++ nesteddatastructures.rb
@@ -0,0 +1,11 @@
+#CREATE A HASH WITH NESTED ARRAYS OF YOUR GROUP'S FAVORITE HASH 
+
+#CREATE A HASH
+
+hello={
+  :Amira => ["Mine","Call Out My Name", "Bartier"],
+  :Alyssa => ["Don't Threaten Me With a Good Time", "Vacation", "Love Lockdown"],
+  :Emi => ["Come on Eileen", "Pitiful Children", "Dancing Queen"]
+}
+puts hello[:Amira][1]
+puts hello[:Alyssa][0]
